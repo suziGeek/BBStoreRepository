fecc43e5f170f44ad923719c2ea99b5da84ea986
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-23 23:46:22 -0400

    adding product repository class

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        public DapperProductRepository()
+        {
+
+        }
+        public void CreateProduct(string name, int price, int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BestBuyBestPractices/IProductRepository.cs
+++ BestBuyBestPractices/IProductRepository.cs
@@ -7,7 +7,7 @@ namespace BestBuyBestPractices
     public interface IProductRepository
     {
         IEnumerable<Product> GetAllProducts();
-        void CreatProduct(string name, int price, int categoryID);
+        void CreateProduct(string name, int price, int categoryID);
 
     }
 }
