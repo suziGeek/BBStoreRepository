cb21dcf08ccb1f96c3ed9f2151272953f4c7b65d
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-23 23:08:41 -0400

    adding product classes

---
+++ BestBuyBestPractices/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuyBestPractices
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        void CreatProduct(string name, int price, int categoryID);
+
+    }
+}
---
+++ BestBuyBestPractices/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace BestBuyBestPractices
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+    }
+}
